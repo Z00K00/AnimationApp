ab13c3d64867f910c560b05e55a40a26d04fda8a
kraiglarson2 <kraigalarson2@gmail.com>
2023-12-19 11:15:01 -0500

    Initial Commit

---
+++ AnimationApp/AnimationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationAppApp.swift
+//  AnimationApp
+//
+//  Created by Krajg Larson on 12/19/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
